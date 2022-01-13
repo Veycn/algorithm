531de28997c7ff5cc9b57a6c6709b2da230d138a
veycn <1594325241@qq.com>
2022-01-13 17:42:46 +0800

    update: 环形链表暴力解法

---
+++ leetcode/editor/cn/[剑指 Offer II 042]最近请求次数.js
@@ -0,0 +1,81 @@
+//写一个 RecentCounter 类来计算特定时间范围内最近的请求。
+//
+// 请实现 RecentCounter 类：
+//
+//
+// RecentCounter() 初始化计数器，请求数为 0 。
+// int ping(int t) 在时间 t 添加一个新请求，其中 t 表示以毫秒为单位的某个时间，并返回过去 3000 毫秒内发生的所有请求数（包括新请求
+//）。确切地说，返回在 [t-3000, t] 内发生的请求数。
+//
+//
+// 保证 每次对 ping 的调用都使用比之前更大的 t 值。
+//
+//
+//
+// 示例：
+//
+//
+//输入：
+//inputs = ["RecentCounter", "ping", "ping", "ping", "ping"]
+//inputs = [[], [1], [100], [3001], [3002]]
+//输出：
+//[null, 1, 2, 3, 3]
+//
+//解释：
+//RecentCounter recentCounter = new RecentCounter();
+//recentCounter.ping(1);     // requests = [1]，范围是 [-2999,1]，返回 1
+//recentCounter.ping(100);   // requests = [1, 100]，范围是 [-2900,100]，返回 2
+//recentCounter.ping(3001);  // requests = [1, 100, 3001]，范围是 [1,3001]，返回 3
+//recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002]，范围是 [2,3002]，返回
+//3
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= t <= 10⁹
+// 保证每次对 ping 调用所使用的 t 值都 严格递增
+// 至多调用 ping 方法 10⁴ 次
+//
+//
+//
+//
+// 注意：本题与主站 933 题相同： https://leetcode-cn.com/problems/number-of-recent-calls/
+// Related Topics 设计 队列 数据流 👍 9 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+var RecentCounter = function () {
+    this.list = []
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+    this.list.push(t)
+    var len = this.list.length, count = 0;
+    for (var i = len -1; i >= 0; i--) {
+        if (this.list[i] >= t - 3000) {
+            count++
+        }
+    }
+    return count
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
+
+var r = new RecentCounter()
+r.ping(1);
+r.ping(100);
+r.ping(3001);
+r.ping(3002);
+//leetcode submit region end(Prohibit modification and deletion)
